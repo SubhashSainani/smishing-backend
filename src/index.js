1410964fa46e13400ccf5d1e1801016265246d9e
SubhashSainani <subhashsainani4@gmail.com>
2025-04-30 18:00:49 +1000

    update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use("/api/auth", authRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server test running on port ${PORT}`);
 });
 
 export default app;
