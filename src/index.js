cd624fb46bef89fc4963f7bb11237b923a449439
SubhashSainani <subhashsainani4@gmail.com>
2025-05-23 20:28:38 +1000

    update index.js

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.get('/health', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server test is running on port ${PORT}`);
 });
 
 export default app;
